af35cedbaffb2ce17a6cfa9444a40a8608ebcde6
Marcos Tulio <87675285+themarcosdev@users.noreply.github.com>
2023-02-08 08:10:50 -0300

    Create nome-idade-input-usuario.sh
    Para executar esse script, você precisa dar permissões de execução para ele usando o comando chmod +x nome_do_arquivo.sh, e depois executá-lo com o comando ./nome_do_arquivo.sh.

---
+++ sh/nome-idade-input-usuario.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Perguntando nome
+echo "Qual seu nome?"
+read nome
+
+# Perguntando idade
+echo "Qual sua idade?"
+read idade
+
+# Exibindo resultados
+echo "Seu nome é $nome e sua idade é $idade."
