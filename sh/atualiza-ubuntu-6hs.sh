1535dc25ec27e85568f3f30533d6c64c79a15a1d
Marcos Tulio <87675285+themarcosdev@users.noreply.github.com>
2023-02-14 09:13:18 -0300

    Correções no cat do arquivo de atualização.
    Dentro do seu user do ubuntu  :
    home/seu-user/
    1- Crie a pasta  :
    home/seu-user/atualizacao_ubuntu
    2- Dentro da pasta 'atualizacao_ubuntu'
    crie o script acima
    3-  Digite o comando :
    ```
    nano ~/.bashrc
    ````
    4 - dentro do arquivo ~/.bashrc na ultima linha cole :
    ```
    # atualizar ubuntu a cada 6 hs
    sh /home/seu-user/atualizacao_ubuntu/atualiza-ubuntu-6hs.sh
    ```

--- sh/atualiza-ubuntu-6hs.sh
+++ sh/atualiza-ubuntu-6hs.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
-# Lê o conteúdo do arquivo ultima_att_ubuntu.txt
-ultima_att=$(cat atualizacao_ubuntu/ultima_att_ubuntu.txt)
+arquivo_valida_atualizacao="/home/$USER/atualizacao_ubuntu/ultima_att_ubuntu.txt"
 
-# Converte o conteúdo do arquivo para o formato de data/hora
+# Lê o conteúdo do arquivo arquivo_valida_atualizacao
+if [ -f $arquivo_valida_atualizacao ]; then
+    ## caso o arquivo exista 
+    ultima_att=$(cat $arquivo_valida_atualizacao)
+else
+  ## caso o arquivo não exista, pegar o momento agora e gravar no arquivo
+  ultima_att=$(date +"%Y/%m/%d %T")
+  echo $ultima_att > $arquivo_valida_atualizacao
+fi
+
+## echo "Ultima verificacao de atualizacao: $ultima_att"
+
+# Converte o conteúdo do arquivo para o formato de data/hora para timestamp
 ultima_att_timestamp=$(date -d "$ultima_att" +%s)
 
-# Obtém a data/hora atual
+# Obtém a data/hora atual em timestamp
 agora=$(date +%s)
 
 # Calcula a diferença entre a data/hora atual e a data/hora do último upgrade
@@ -15,9 +26,9 @@ diferenca=$((agora - ultima_att_timestamp))
 # Verifica se a diferença é maior que 6 horas (6 horas em segundos é 21600)
 if [ $diferenca -gt 21600 ]; then
   echo 'Atualize seu ubuntu : '
-  # Executa o upgrade
-  sudo apt update
-  sudo apt upgrade
-  # Cria o arquivo ultima_att_ubuntu.txt ou atualiza seu conteúdo
-  echo $(date +"%Y/%m/%d %T") > atualizacao_ubuntu/ultima_att_ubuntu.txt
+  # Executa o update & upgrade
+  sudo apt update -y
+  sudo apt upgrade -y
+  # Cria o arquivo arquivo_valida_atualizacao ou atualiza seu conteúdo
+  echo $(date +"%Y/%m/%d %T") > $arquivo_valida_atualizacao
 fi
