6a08713e24bbbcad2b6e3fef1e736fe474c0e734
Marcos Tulio <87675285+themarcosdev@users.noreply.github.com>
2023-05-10 17:13:56 -0300

    Desativando a validação da lib openssl .

--- sh/enc_dec/enc_dec.sh
+++ sh/enc_dec/enc_dec.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Verifica se a biblioteca OpenSSL está instalada
-if ! command -v openssl &> /dev/null
-then
-    echo "A biblioteca OpenSSL não está instalada. Por favor, instale-a antes de usar este script."
-    exit
-fi
+#if ! command -v openssl &> /dev/null
+#then
+#    echo "A biblioteca OpenSSL não está instalada. Por favor, instale-a antes de usar este script."
+#    exit
+#fi
 
-# Parâmetros 
+# Parâmetros
 string=$1
 salt=$2
 codificar_decodificar=$3
 
-# Codificar / Decodificar 
+# Codificar / Decodificar
 if [ "$codificar_decodificar" = "codificar" ]; then
   echo $string | openssl enc -aes-256-cbc -a -salt -pbkdf2 -pass pass:$salt
 elif [ "$codificar_decodificar" = "decodificar" ]; then
